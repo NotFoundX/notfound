043cd14e122b9da7ff38c2e1faeacbb7b10ecec0
LX <luisencarnacion772@gmail.com>
2019-02-04 22:06:45 -0400

    OPC 1, ENVIAR GMAIL, SE AGREGO UN NUEVO CODIGO

---
+++ modules/banner.py
@@ -0,0 +1,8 @@
+from colorama import Fore
+import time
+print(Fore.WHITE +  ' _   _  ___ _____   _____ ___  _   _ _   _ ____')
+print (Fore.WHITE + '| \ | |/ _ \_   _| |  ___/ _ \| | | | \ | |  _ \ ')
+print (Fore.WHITE + '|  \| | | | || |   | |_ | | | | | | |  \| | | | |')
+print (Fore.WHITE + '| |\  | |_| || |   |  _|| |_| | |_| | |\  | |_| |')
+print (Fore.WHITE + '|_| \_|\___/ |_|   |_|   \___/ \___/|_| \_|____/')
+time.sleep(2)
--- modules/gmail.py
+++ modules/gmail.py
@@ -1,14 +1,19 @@
 import smtplib
 import config
 import time
+from email.mime.text import MIMEText
 from colorama import Fore
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(config.correo, config.password)
-mensaje = 'From: %s\nTo: %s\n\n' % (config.correo, config.vic)
-mensaje += input(Fore.RED + 'INSERTA EL TEXTO >> ')
-server.sendmail(config.correo, config.vic,mensaje)
+mensaje = input(Fore.GREEN + 'INSERT EL TEXTO >> ')
+print()
+mime_message = MIMEText(mensaje, "plain", _charset="utf-8")
+mime_message["From"] = config.correo
+mime_message["To"] = config.vic
+mime_message["Subject"] = input(Fore.GREEN + 'INSERT ASUNT >> ')
+server.sendmail(config.correo, config.vic, mime_message.as_string())
 time.sleep(3)
 print()
-print (Fore.GREEN + 'SUCESSUL DONE!!!')
+print (Fore.GREEN + 'SENT WITH SUCCESS!!!')
 server.quit()
